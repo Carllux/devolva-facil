f54c44209999d8d422c61b2614972c315d85fde1
Carllux <82111083+Carllux@users.noreply.github.com>
2026-06-08 10:30:17 -0300

    Implement routing in App component
    Refactor App component to use React Router for navigation and add new routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importando o Roteador
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
-import FloatingWhatsApp from './components/FloatingWhatsApp'; // <-- Adicione esta importação
+import FloatingWhatsApp from './components/FloatingWhatsApp';
+import TermosUso from './pages/TermosUso';
+import PoliticaPrivacidade from './pages/PoliticaPrivacidade';
+import SegurancaInformacao from './pages/SegurancaInformacao';
 
 export default function App() {
   return (
-    <div className="light min-h-screen flex flex-col justify-between relative">
-      <Header />
-      <Home />
-      <Footer />
-      
-      {/* Componente flutuante global */}
-      <FloatingWhatsApp /> 
-    </div>
+    <Router>
+      <div className="light min-h-screen flex flex-col justify-between relative">
+        <Header />
+        
+        {/* Rotas configuradas */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/termos-de-uso" element={<TermosUso />} />
+          <Route path="/politica-de-privacidade" element={<PoliticaPrivacidade />} />
+          <Route path="/seguranca-da-informacao" element={<SegurancaInformacao />} />
+        </Routes>
+        
+        <Footer />
+        <FloatingWhatsApp /> 
+      </div>
+    </Router>
   );
 }
